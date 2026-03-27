30869d27027d4226680e206d5d72f64b4256144e
Muhammed Athimannil <hello@athimannil.com>
2026-03-27 17:22:43 +0100

    feat: Update RootLayout to simplify structure and add middleware for redirection

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { ReactNode } from 'react';
 import './globals.css';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
   title: 'Globuss Solutions',
@@ -19,16 +9,8 @@ export const metadata: Metadata = {
 
 export default async function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+}: {
+  children: ReactNode;
+}) {
+  return <>{children}</>;
 }
